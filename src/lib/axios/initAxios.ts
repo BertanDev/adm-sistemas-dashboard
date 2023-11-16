19a571a4d980732df6b84f4238895739f0b54d66
João Vitor Bertan <72395637+BertanDev@users.noreply.github.com>
2023-11-15 22:47:36 -0300

    t

--- src/lib/axios/initAxios.ts
+++ src/lib/axios/initAxios.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const api = axios.create({
   // baseURL: 'http://localhost:8080/',
-  baseURL: 'https://2771-179-127-134-28.ngrok.io',
+  baseURL: 'https://28ac-179-127-134-28.ngrok.io',
 })
 
 export { api }
