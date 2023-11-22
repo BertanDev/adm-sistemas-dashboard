3443152f1c45bb3bcc32250b5664cffc023daf7b
João Vitor Bertan <72395637+BertanDev@users.noreply.github.com>
2023-11-22 20:52:24 -0300

    baseURL for depploy

--- src/lib/axios/initAxios.ts
+++ src/lib/axios/initAxios.ts
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080/',
+  baseURL: 'https://adm-dashboard-api.onrender.com',
+  // baseURL: 'http://localhost:8080/',
   // baseURL: 'https://28ac-179-127-134-28.ngrok.io',
 })
 
