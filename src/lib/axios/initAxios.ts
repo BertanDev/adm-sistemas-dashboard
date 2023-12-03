ecb3634717704ad327158864419486450bdf9d14
João Vitor Bertan <72395637+BertanDev@users.noreply.github.com>
2023-12-03 18:59:34 -0300

    test deploy

--- src/lib/axios/initAxios.ts
+++ src/lib/axios/initAxios.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const api = axios.create({
   // baseURL: 'https://adm-dashboard-api.onrender.com',
-  baseURL: 'http://18.230.60.191/:8080',
+  baseURL: 'http://18.230.60.191:8080',
   // baseURL: 'https://28ac-179-127-134-28.ngrok.io',
 })
 
